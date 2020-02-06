188411da3686baf0cd34de17d72551f59d6caa6d
aott33 <ott.andrew33@gmail.com>
2020-02-06 01:03:50 -0800

    Opens CSV file and prints each row

--- FinTracker_Simple.py
+++ FinTracker_Simple.py
@@ -1,9 +1,20 @@
+import csv
+
 def main():
     """Definition of the main function.
+        Opens a CSV and calls other functions to format list
 
     :return:void
     :rtype: void
 
     """
+    capitalCSV = open("../test.csv")
+    capitalReader = csv.reader(capitalCSV)
+
+    monthlyTotal = 0;
+
+    for row in capitalReader:
+        print(str(capitalReader.line_num) + ' ' + str(row))       
+
 
 main()
