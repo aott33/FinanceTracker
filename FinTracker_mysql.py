7f10b8b2f27eb2c801bcfd3024f694c0e4102190
aott33 <ott.andrew33@gmail.com>
2020-02-12 06:44:18 -0800

    Practicing with mysql and python

--- FinTracker_mysql.py
+++ FinTracker_mysql.py
@@ -3,7 +3,20 @@ import mysql.connector
 cnx = mysql.connector.connect(
     host="localhost",
     user="andrew",
-    passwd=""
+    passwd="andrew",
+    database="testdb"
     )
 
-cnx.close()
+mycursor = cnx.cursor()
+
+
+#mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age SMALLINT UNSIGNED,\
+#                personID int PRIMARY KEY AUTO_INCREMENT)")
+
+mycursor.execute("INSERT INTO Person (name, age) VALUES(%s, %s)", ("Bob", 20))
+cnx.commit()
+
+mycursor.execute("SELECT * FROM Person")
+
+for x in mycursor:
+    print(x)
