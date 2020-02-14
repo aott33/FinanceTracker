c46541347004e44bc781002882d51c0aba215e92
aott33 <ott.andrew33@gmail.com>
2020-02-14 02:54:30 -0800

    Trying to solve a bug

--- FinTracker_mysql.py
+++ FinTracker_mysql.py
@@ -9,12 +9,17 @@ cnx = mysql.connector.connect(
 
 mycursor = cnx.cursor()
 
+# Changes character set of database
+# mycursor.execute("ALTER DATABASE testdb CHARACTER SET utf8 COLLATE utf8_general_ci")
 
-#mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age SMALLINT UNSIGNED,\
+
+# mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age SMALLINT UNSIGNED,\
 #                personID int PRIMARY KEY AUTO_INCREMENT)")
 
-mycursor.execute("INSERT INTO Person (name, age) VALUES(%s, %s)", ("Bob", 20))
-cnx.commit()
+# Example how to add items to the table
+# mycursor.execute("INSERT INTO Person (name, age) VALUES(%s, %s)", ("Bob", 20))
+
+#cnx.commit()
 
 mycursor.execute("SELECT * FROM Person")
 
