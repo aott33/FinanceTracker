d904ab74ff302ecf42bd0d78ceded3d4f957eb6e
aott33 <ott.andrew33@gmail.com>
2020-02-15 02:56:29 -0800

    Creates a table, inserts data, then prints table

--- mysql_practice.py
+++ mysql_practice.py
@@ -10,6 +10,14 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-mycursor.execute("CREATE TABLE Test(name VARCHAR(50) NOT NULL,\
-                created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL,\
-                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+# mycursor.execute("CREATE TABLE Test(name VARCHAR(50) NOT NULL,\
+#                created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL,\
+#                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+
+# mycursor.execute("INSERT INTO Test (name, created, gender) VALUES (%s,%s,%s)",
+#                ("Mary", datetime.now(), "F"))
+
+mycursor.execute("SELECT * FROM Test WHERE gender='M' ORDER BY id DESC")
+
+for x in mycursor:
+    print(x)
